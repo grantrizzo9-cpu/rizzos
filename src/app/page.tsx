ae62645f24e8b911d455001d209a84bc2cf92531
Grant Rizzoli <rentapog@gmail.com>
2026-02-20 05:57:34 +0000

    can you LSO EXPLAIN TO THEM THAT IF THEY HAVE BEEN PAID OUT from there r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,7 @@ const faqs = [
   },
   {
     question: "Can I get a refund on the activation fee?",
-    answer: "Yes. We offer a no-questions-asked refund on your one-time activation fee if you request it within 24 hours of joining. We want you to be happy. If you decide it's not for you, just let us know, and we'll process it with no hard feelings. Please note that if you choose to receive a refund, your account will be closed, and you will no longer be able to log in.",
+    answer: "Yes. We offer a no-questions-asked refund on your one-time activation fee if you request it within 24 hours of joining, with one important condition: to be eligible for a refund, you must not have received any commission payouts from your referrals. If you have been paid out, a refund is not possible. If you decide it's not for you (and haven't received payouts), just let us know, and we'll process it with no hard feelings. Please note that if you choose to receive a refund, your account will be closed, and you will no longer be able to log in.",
   }
 ];
 
